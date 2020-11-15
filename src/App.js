4c05427bd778c1aaef5a57fdccd8a0820d039e3a
Nariman Etemadi <n.etemadi@intermax.nl>
2020-11-15 02:10:01 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,87 @@
-import logo from './logo.svg';
+import {ReactComponent as Logo} from './assets/LoginButton.svg';
 import './App.css';
+import { motion } from "framer-motion"
+import { state } from "framer-motion"
+import React, { useState } from 'react';
+import detectEthereumProvider from '@metamask/detect-provider';
+
+const ContainerVariants = {
+  initial: {},
+  animate: {}
+};
+const H1Variants = {
+  initial: { x: -1000 },
+  animate: { x: 0 },
+  transition: {
+    type: "tween",
+    duration: 2,
+    delay: 1
+  }
+}
+const H2Variants = {
+  initial: { y: -1000 },
+  animate: { y: 0 },
+  transition: {
+    type: "tween",
+    duration: 1,
+    delay: .4
+  }
+}
+const H3Variants = {
+  initial: { x: 100, opacity: 0 },
+  animate: { x: 0, opacity: 1 }
+}
+const H4Variants = {
+  initial: { scale: 0.7 },
+  animate: { scale: 1.7 },
+  transition: {
+    type: "tween",
+    duration: "2",
+    delay: "1"
+  }
+}
 
 function App() {
+  const ethereum = window.ethereum
+  const [addr, setAddr] = useState('')
+  if (ethereum) {
+    ethereum.on('accountsChanged', function (accounts) {
+      console.log(accounts[0])
+      setAddr(accounts[0]);
+    })
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <motion.div
+          className="App"
+          variants={ContainerVariants}
+          initial="initial"
+          animate="animate"
         >
-          Learn React
-        </a>
+          <motion.h1 variants={H1Variants}>
+            <div Hello>
+              <h1 className="App-login">Hello user</h1>
+            </div>
+          </motion.h1>
+          <motion.h2 variants={H2Variants}>
+            <div User public key>
+            <h1 className="App-Address"> {addr}</h1>
+            </div>
+          </motion.h2>
+          <motion.h3 variants={H3Variants}>
+            <div>STARSSSS</div>
+          </motion.h3>
+          <motion.h4 variants={H4Variants}>
+            <div>STARSSSS</div>
+            </motion.h4>
+        </motion.div>
+        <div Login button>
+        <button> <Logo className='MetaLogin' /> </button>
+        </div>
       </header>
+      <main>
+      </main>
     </div>
   );
 }
