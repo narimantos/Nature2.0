63cc881d41da6217d3ef2ea60adc5f93256defac
Nariman Etemadi <n.etemadi@intermax.nl>
2020-11-15 05:12:53 +0100

    MetaMask address get

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import {ReactComponent as Logo} from './assets/LoginButton.svg';
+import { ReactComponent as Logo } from './assets/LoginButton.svg';
 import './App.css';
+import Web3 from 'web3';
 import { motion } from "framer-motion"
 import { state } from "framer-motion"
-import React, { useState } from 'react';
+import React, { Component, useState } from 'react';
 import detectEthereumProvider from '@metamask/detect-provider';
+
+
+//const [addr, setAddr] = useState('')
+
+ const ethEnabled = () => {
+  if (window.web3) {
+    window.web3 = new Web3(window.web3.currentProvider);
+    window.ethereum.enable();
+    return true;
+  }
+  return false;
+}
+
+var web3
 
 const ContainerVariants = {
   initial: {},
@@ -32,58 +47,96 @@ const H3Variants = {
   animate: { x: 0, opacity: 1 }
 }
 const H4Variants = {
-  initial: { scale: 0.7 },
-  animate: { scale: 1.7 },
+  initial: { scale: 1.4 },
+  animate: { scale: 1.4 },
   transition: {
     type: "tween",
-    duration: "2",
+    duration: "4",
     delay: "1"
   }
 }
 
-function App() {
-  const ethereum = window.ethereum
-  const [addr, setAddr] = useState('')
-  if (ethereum) {
-    ethereum.on('accountsChanged', function (accounts) {
-      console.log(accounts[0])
-      setAddr(accounts[0]);
-    })
+
+class App extends Component {
+  _isMounted = false;
+
+  state = {
+    account: "nothing"
   }
-  return (
-    <div className="App">
-      <header className="App-header">
-        <motion.div
-          className="App"
-          variants={ContainerVariants}
-          initial="initial"
-          animate="animate"
-        >
-          <motion.h1 variants={H1Variants}>
-            <div Hello>
-              <h1 className="App-login">Hello user</h1>
-            </div>
-          </motion.h1>
-          <motion.h2 variants={H2Variants}>
-            <div User public key>
-            <h1 className="App-Address"> {addr}</h1>
-            </div>
-          </motion.h2>
-          <motion.h3 variants={H3Variants}>
-            <div>STARSSSS</div>
-          </motion.h3>
-          <motion.h4 variants={H4Variants}>
-            <div>STARSSSS</div>
+  constructor(props) {
+    super(props)
+    this.state = { account: 'NOT YET DEFINED'}
+  }
+  componentDidMount() {
+    setInterval(() => {
+        this.setState(() => {
+            console.log('setting state');
+            this.render()
+            return { unseen: "does not display" }
+        });
+    }, 1000);
+}
+shouldComponentUpdate(nextProps, nextState) {
+  return this.state.account != nextProps.account 
+}
+
+// changePageState(e){
+//   this.setState({ page: e.target.value });
+//   this.getPage();
+// }
+
+ethereum = window.ethereum
+
+if(ethereum)
+{
+  ethereum.on('accountChanged',
+  function (accounts)
+  {
+    this.setState.account = accounts[0]
+  }
+  )
+}
+
+  async getMetaAccount() {
+        web3 =  new Web3(window.web3.currentProvider)
+        var account = web3.eth.getAccounts().then(result =>  
+          {
+              console.log(result);
+              this.setState({account: result[0]})
+          });
+ 
+      console.log("Your Account IS = "+ this.state.account)
+    }
+   
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <motion.div
+            className="App"
+            variants={ContainerVariants}
+            initial="initial"
+            animate="animate"
+          >
+            <motion.h2 variants={H2Variants}>
+              <div>
+              Hello,  {this.state.account} 
+              </div>
+            </motion.h2>
+            <motion.h3 variants={H3Variants}>
+              <div>STARS</div>
+            </motion.h3>
+            <motion.h4 variants={H4Variants}>
+              <div>STARS</div>
             </motion.h4>
-        </motion.div>
-        <div Login button>
-        <button> <Logo className='MetaLogin' /> </button>
-        </div>
-      </header>
-      <main>
-      </main>
-    </div>
-  );
+          </motion.div>
+            <button onClick={this.getMetaAccount.bind(this)}> <Logo/> </button>
+        </header>
+        <main>
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
