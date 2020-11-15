672280a01be9be83a3db86f9faa56032fbd3c82e
Nariman Etemadi <n.etemadi@intermax.nl>
2020-11-15 06:25:26 +0100

    login button

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import { ReactComponent as Logo } from './assets/LoginButton.svg';
+import { ReactComponent as Logout } from './assets/Logout.svg';
 import './App.css';
 import Web3 from 'web3';
-import { motion } from "framer-motion"
+import { motion, useMotionValue } from "framer-motion"
 import { state } from "framer-motion"
 import React, { Component, useState } from 'react';
 import detectEthereumProvider from '@metamask/detect-provider';
@@ -9,7 +10,7 @@ import detectEthereumProvider from '@metamask/detect-provider';
 
 //const [addr, setAddr] = useState('')
 
- const ethEnabled = () => {
+const ethEnabled = () => {
   if (window.web3) {
     window.web3 = new Web3(window.web3.currentProvider);
     window.ethereum.enable();
@@ -18,6 +19,18 @@ import detectEthereumProvider from '@metamask/detect-provider';
   return false;
 }
 
+const isLoggedIn = (object) => {
+if(object.state.gotMM)
+{ 
+  return true
+}
+else 
+{
+  console.log("NOT TRUE")
+
+  return false
+}
+}
 var web3
 
 const ContainerVariants = {
@@ -41,6 +54,7 @@ const H2Variants = {
     duration: 1,
     delay: .4
   }
+
 }
 const H3Variants = {
   initial: { x: 100, opacity: 0 },
@@ -57,58 +71,71 @@ const H4Variants = {
 }
 
 
+
 class App extends Component {
   _isMounted = false;
 
   state = {
-    account: "nothing"
+    account: "nothing",
+    gotMM: 0
   }
+
   constructor(props) {
     super(props)
-    this.state = { account: 'NOT YET DEFINED'}
+    this.state = { account: 'NOT YET DEFINED',gotMM: 0 }
   }
   componentDidMount() {
     setInterval(() => {
-        this.setState(() => {
-            console.log('setting state');
-            this.render()
-            return { unseen: "does not display" }
-        });
+      this.setState(() => {
+        console.log('setting state');
+        this.render()
+        return { unseen: "does not display" }
+      });
     }, 1000);
-}
-shouldComponentUpdate(nextProps, nextState) {
-  return this.state.account != nextProps.account 
-}
-
-// changePageState(e){
-//   this.setState({ page: e.target.value });
-//   this.getPage();
-// }
-
-ethereum = window.ethereum
-
-if(ethereum)
-{
-  ethereum.on('accountChanged',
-  function (accounts)
-  {
-    this.setState.account = accounts[0]
   }
-  )
-}
+  shouldComponentUpdate(nextProps, nextState) {
+    return this.state.account != nextProps.account
+  }
+
+  // changePageState(e){
+  //   this.setState({ page: e.target.value });
+  //   this.getPage();
+  // }
+
+  ethereum = window.ethereum
+
+  if(ethereum) {
+    ethereum.on('accountChanged',
+      function (accounts) {
+        this.setState.account = accounts[0]
+      }
+    )
+  }
 
   async getMetaAccount() {
-        web3 =  new Web3(window.web3.currentProvider)
-        var account = web3.eth.getAccounts().then(result =>  
-          {
-              console.log(result);
-              this.setState({account: result[0]})
-          });
- 
-      console.log("Your Account IS = "+ this.state.account)
-    }
-   
+    web3 = new Web3(window.web3.currentProvider)
+    var account = web3.eth.getAccounts().then(result => {
+      console.log(result);
+      this.setState({ account: result[0], gotMM: 1 })
+    });
+  
+  }
+
+  async logoutMeta()
+  {
+    this.setState({account: "Undifined", gotMM: 0})
+  }
+
+
+  handelLoginClick(){}
+  handelLogOutClick(){}
   render() {
+    let button;
+
+
+
+
+
     return (
       <div className="App">
         <header className="App-header">
@@ -120,20 +147,19 @@ if(ethereum)
           >
             <motion.h2 variants={H2Variants}>
               <div>
-              Hello,  {this.state.account} 
+                {isLoggedIn(this)   && <p> Hello, {this.state.account}  </p>}
+                {!isLoggedIn(this)  && <p> Hello {this.state.account} please login </p>}
               </div>
             </motion.h2>
             <motion.h3 variants={H3Variants}>
-              <div>STARS</div>
+              <div className="App-Stars">✰⋆🌟✪🔯✨</div >
             </motion.h3>
-            <motion.h4 variants={H4Variants}>
-              <div>STARS</div>
-            </motion.h4>
-          </motion.div>
-            <button onClick={this.getMetaAccount.bind(this)}> <Logo/> </button>
+          </motion.div> <div >
+             {isLoggedIn(this)   && <button onClick={this.logoutMeta.bind(this)}> <Logout className="App-Stars" >  </Logout>  </button> }  
+             {!isLoggedIn(this)   && <button onClick={this.getMetaAccount.bind(this)}> <Logo className="App-Stars" >  </Logo>  </button> }  
+
+          </div>
         </header>
-        <main>
-        </main>
       </div>
     );
   }
